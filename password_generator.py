03364908db9a9ed06178df520cabbb94f87540a0
Parisa <20560187+Parisa-lab@users.noreply.github.com>
2024-02-10 22:23:35 +0330

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -14,9 +14,40 @@ def generate_password(length):
     # Generate a password using a random selection of characters
     for i in range(length):
         password = password + random.choice(pass_chars)
-    return password
+    
+    strength_checker(password)
 
-length = int(input("How Long do You Want Your Password to be? "))
-password = generate_password(length)
-print(password)
+
+def strength_checker(password):
+    special_chars = string.punctuation
+
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+    has_special_char = False
+    
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+        elif char in special_chars:
+            has_special_char = True
+
+    # Check if the password is strong enough
+    if has_uppercase and has_lowercase and has_digit and has_special_char:
+        print("Your Secure Password is: " + password)
+    else:
+        generate_password(len(password))
+
+length = int(input("How Long do You Want Your Password to be? It Must be at Least 8 Characters. "))
+
+while True:
+    if length >= 8:
+        password = generate_password(length)
+        break
+    else:
+        length = int(input("Type a Number Greater than 8. "))
 
